b32c1da602dbfd81ec0986b0d46539954bda5bae
Alexander Kazanski <alexanderkazanski@outlook.com>
2024-11-29 14:52:26 -0500

    debug

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,12 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-export default defineConfig({
+export default defineConfig(() => ({
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        // Add other HTML files here
-        "oldgram": resolve(__dirname, 'portfolio-item-oldagram.html'),
-        "tenzies": resolve(__dirname, 'portfolio-item-tenzies.html'),
-        "password-generator": resolve(__dirname, 'portfolio-item-password-generator.html'),
-        "meme-generator": resolve(__dirname, 'portfolio-item-meme-generator.html'),
-        "quiz-app": resolve(__dirname, 'portfolio-item-quiz-app.html'),
-        "personal-dashboard": resolve(__dirname, 'under-development.html'),
-        css: resolve(__dirname, './css/style.css'),
-        'index.js': resolve(__dirname, './js/index.js')
-
-        // Add more as needed
-      },
+      input: [
+        resolve(__dirname, 'index.html'),
+      ],
     },
   },
-})
+}));
