410ce6e8e4cbba88594f9af26572a7f71221731d
Alexander Kazanski <alexanderkazanski@outlook.com>
2024-11-29 14:37:45 -0500

    fix presentation

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,14 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         // Add other HTML files here
-        about: resolve(__dirname, 'portfolio-item-password-generator.html'),
+        "oldgram": resolve(__dirname, 'portfolio-item-oldagram.html'),
+        "tenzies": resolve(__dirname, 'portfolio-item-tenzies.html'),
+        "password-generator": resolve(__dirname, 'portfolio-item-password-generator.html'),
+        "meme-generator": resolve(__dirname, 'portfolio-item-meme-generator.html'),
+        "quiz-app": resolve(__dirname, 'portfolio-item-quiz-app.html'),
+        "personal-dashboard": resolve(__dirname, 'under-development.html'),
+        css: resolve(__dirname, './css/style.css'),
+        js: resolve(__dirname, './js/index.js')
 
         // Add more as needed
       },
