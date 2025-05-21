3636780f0ff862c0e874eb2608c6d60c5e2bb46b
Alexander Kazanski <alexander1.15.1943@gmail.com>
2025-05-21 19:48:21 -0400

    Implement code changes to enhance functionality and improve performance

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const nav = document.querySelector('.nav');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
+// Close mobile menu when clicking a link
+nav.addEventListener('click', e => {
+    if (e.target.classList.contains('nav__link')) {
         document.body.classList.remove('nav-open');
-    })
-})
+    }
+});
+
+// Enable smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
